e115af3602ecc2503dfc0a9fe2bdda4e480b1db8
mohammad <mrmprogram26379@gmail.com>
2024-01-06 23:16:50 +0330

    AddAutoMapper

---
+++ HRLeaveManagement.Application/ApplicationSevicesRegistration.cs
@@ -0,0 +1,19 @@
+﻿using HRLeaveManagement.Application.Profiles;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagement.Application
+{
+    public static class ApplicationSevicesRegistration
+    {
+        public static void ConfigureApplicationSevices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        }
+    }
+}
