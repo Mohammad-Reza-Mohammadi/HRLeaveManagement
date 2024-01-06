55ca56f254513c1396158e0db9b9b20213d8840d
mohammad <mrmprogram26379@gmail.com>
2024-01-06 18:41:05 +0330

    Create Domain And Repository

---
+++ HRLeaveManagement.Domain/Common/BaseDomainEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace HRLeaveManagement.Domain.Common
+{
+    public abstract class BaseDomainEntity
+    {
+        public int id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string CreateBy { get; set; }
+        public string LastModifiedDate { get; set; }
+        public string  LastModifiedBy { get; set; }
+    }
+}
---
+++ HRLeaveManagement.Domain/LeaveType.cs
@@ -0,0 +1,8 @@
+﻿namespace HRLeaveManagement.Domain
+{
+    public class LeaveType
+    {
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+    }
+}
