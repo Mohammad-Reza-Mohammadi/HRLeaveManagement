2c3ad5ad8cb8cf8e4d14d8ac0b00e8e5ed178436
mohammad <mrmprogram26379@gmail.com>
2024-01-07 09:15:10 +0330

    Add mediatR and CQRS

--- HRLeaveManagement.Application/ApplicationSevicesRegistration.cs
+++ HRLeaveManagement.Application/ApplicationSevicesRegistration.cs
@@ -1,19 +1,17 @@
-﻿using HRLeaveManagement.Application.Profiles;
+﻿using MediatR;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HRLeaveManagement.Application
 {
     public static class ApplicationSevicesRegistration
     {
-        public static void ConfigureApplicationSevices(this IServiceCollection services)
+        public static IServiceCollection ConfigureApplicationSevices(this IServiceCollection services)
         {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
         }
     }
 }
---
+++ HRLeaveManagement.Application/Features/LeaveTypes/Handlers/Queries/GetLeaveTypeListRequestHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using HRLeaveManagement.Application.DTOs;
+using HRLeaveManagement.Application.Features.LeaveTypes.Requests.Queries;
+using HRLeaveManagement.Application.Persistence.Contracts;
+using MediatR;
+
+namespace HRLeaveManagement.Application.Features.LeaveTypes.Handlers.Queries
+{
+    public class GetLeaveTypeListRequestHandler : IRequestHandler<GetLeaveTypeListRequest, List<LeaveTypeDto>>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly IMapper _mapper;
+
+        public GetLeaveTypeListRequestHandler(ILeaveTypeRepository leaveTypeRepository, IMapper mapper)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+            _mapper = mapper;
+        }
+        public async Task<List<LeaveTypeDto>> Handle(GetLeaveTypeListRequest request, CancellationToken cancellationToken)
+        {
+            var LeaveTypes = await _leaveTypeRepository.GetAllAsync();
+            return _mapper.Map<List<LeaveTypeDto>>(LeaveTypes);
+        }
+    }
+}
