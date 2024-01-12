9ca0c6baa7780db998a75e3d2836b118519bb1bf
mohammad <mrmprogram26379@gmail.com>
2024-01-12 22:22:18 +0330

    Add entity famework

---
+++ HRLeaveManagement.Persistence/HrLeaveManagementDbContext.cs
@@ -0,0 +1,42 @@
+﻿using HRLeaveManagement.Domain;
+using HRLeaveManagement.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagement.Persistence
+{
+    public class HrLeaveManagementDbContext : DbContext
+    {
+        public HrLeaveManagementDbContext(DbContextOptions<HrLeaveManagementDbContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(HrLeaveManagementDbContext).Assembly);
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseDomainEntity>())
+            {
+                entry.Entity.LastModifiedDate = DateTime.Now.ToString();
+
+                if(entry.State == EntityState.Added)
+                {
+                    entry.Entity.DateCreated = DateTime.Now;
+                }
+            } 
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+        public DbSet<LeaveType> leaveTypes { get; set; }
+        public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+    }
+}
